8c6bbc57a22aa50c1c3230c91a35da2c608e6f7c
«Irina <ira87m@mail.ru>
2023-02-10 22:36:05 +0300

    Task 07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -5,4 +5,12 @@
 // 782 -> 2
 // 918 -> 8
 
-
+Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+   int lastNumber = number % 10;
+   Console.Write($"Последняя цифра числа  {lastNumber}");
+}
+else 
+  Console.Write("Вы ввели не верное число");
