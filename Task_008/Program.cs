9b9c9ab7ea15dace166dba1ef1b4651acb8d8407
Irina Melianiuk <ira87m@mail.ru>
2023-02-13 16:54:04 +0300

    Task 008 changed

--- Task_008/Program.cs
+++ Task_008/Program.cs
@@ -6,23 +6,14 @@
 Console.Write("Введите число N - ");
 int number = Convert.ToInt32(Console.ReadLine());   
 int index = 1;
-for (int index <= number; index%2 == 0; index++)
- {
-   //if (index%2 == 0)
-     Console.Write(index);
-   //else
-   //  index++;
- }
-
-//if (index <= number) //&& index%2 == 0)
-  //if (index%2 == 0)
- // {
-  //  index%2 == 0;
-   // Console.Write(index);
-    //index++;
- // }
-//else 
-   // index++;
-//else
-//  Console.Write(index);
+while (index <= number)
+{
+    if (index%2 == 0)
+    {
+       Console.Write($"{index} ");       
+    }
+    index++;
+}
+    
  
+
