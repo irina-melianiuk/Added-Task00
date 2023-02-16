70e8f7d930a473c8de570d8164a138c5aaf28f3d
Irina Melianiuk <ira87m@mail.ru>
2023-02-16 14:25:41 +0300

    Task 11

---
+++ Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 -- 46
+// 782 -- 72
+// 918 -- 98
+
+int number = new Random().Next(100, 1000);  
+ Console.WriteLine($"Случайное число из отрезка 100 - 999 => {number}");
+
+ int resDigit = ResultDigit(number);
+ Console.WriteLine($"Результат = {resDigit}");
+ 
+int ResultDigit(int num)
+{
+  int firstDigit = num / 100;  
+  int thirdDigit = num % 10;
+
+  return (firstDigit*10 + thirdDigit);
+}
