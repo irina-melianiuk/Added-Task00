e992194e0dd8641035b8043686f0b0bb9e1b8352
Irina Melianiuk <ira87m@mail.ru>
2023-02-16 12:53:12 +0300

    Task 09 changed

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -6,11 +6,31 @@
 // 85 -> 8
 
 
-int number = new Random().Next(10, 100);  // 99+1т.к последнее число диапозона не войдет
-Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
-int firstDigit = number / 10;  // целочисленное деление
-int secondDigit = number % 10;  // остаток от деления на 10
-if (firstDigit > secondDigit)
- Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
-else 
-  Console.WriteLine($"Наибольшая цифра числа = {secondDigit}");
+ int number = new Random().Next(10, 100);  // 99+1т.к последнее число диапозона не войдет
+ Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+ 
+// int firstDigit = number / 10;  // целочисленное деление
+// int secondDigit = number % 10;  // остаток от деления на 10
+// if (firstDigit > secondDigit)
+//  Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
+// else 
+//   Console.WriteLine($"Наибольшая цифра числа = {secondDigit}");
+
+
+int maxDigit = MaxDigit(number);
+ Console.WriteLine($"Наибольшая цифра числа = {maxDigit}");
+ 
+int MaxDigit(int num)
+{
+  int firstDigit = num / 10;  
+  int secondDigit = num % 10;
+
+ // if (firstDigit > secondDigit) return firstDigit;
+ // return secondDigit;
+
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// return result;
+
+
+return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
