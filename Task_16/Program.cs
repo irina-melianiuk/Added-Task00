076dcd864674850908cfc6b8f72b270804a4dfdc
«Irina <ira87m@mail.ru>
2023-02-16 18:20:41 +0300

    Task 16 changed

--- Task_16/Program.cs
+++ Task_16/Program.cs
@@ -4,3 +4,19 @@
 // 25,5 -- да
 //8,9 -- нет
 
+Console.Write("Введите число 1 - ");
+int number1 = Convert.ToInt32(Console.ReadLine());   
+Console.Write("Введите число 2 - ");
+int number2 = Convert.ToInt32(Console.ReadLine()); 
+
+bool Result(int num1, int num2)
+{
+ int a = num1*num1;
+ int b = num2*num2;
+  return (a == num2 | b == num1);
+}
+
+if (Result(number1,number2)) 
+  Console.WriteLine($"да");
+else
+  Console.WriteLine($"нет");
