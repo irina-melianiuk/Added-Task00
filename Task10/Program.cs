5d6b78187a718e2143f7860c8f73229338ec71f6
Irina Melianiuk <ira87m@mail.ru>
2023-02-17 09:50:16 +0300

    Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,3 +3,16 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+int number = new Random().Next(100, 1000);  
+ Console.WriteLine($"Случайное число из отрезка 100 - 999 => {number}");
+
+ int resDigit = ResultDigit(number);
+ Console.WriteLine($"Вторая цифра числа = {resDigit}");
+ 
+int ResultDigit(int num)
+{
+  int firstDigit = num / 10;  
+  
+  return (firstDigit % 10);
+}
