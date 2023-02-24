79f2a601efd99bc3785addfb37b90d2e23c7a929
«Irina <ira87m@mail.ru>
2023-02-24 22:52:15 +0300

    Task 38

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -1,5 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
 
 
