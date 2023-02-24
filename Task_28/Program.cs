cc73fcd01a45172cf5efeec70c58f8c1a7237fd2
Irina Melianiuk <ira87m@mail.ru>
2023-02-24 16:49:39 +0300

    Task 28

---
+++ Task_28/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28
+//Напишите программу, которая принимает на вход число N  и выдает роизведение чисел от 1 до N.
+// 4 -- 24
+// 5 -- 120
+
+Console.Write("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int number = new Random().Next(1, 10);  
+// Console.WriteLine($"Случайное число => {number}");
+
+int result = Multiplication(number);
+Console.WriteLine($"Результат умножения = {result}");
+
+int Multiplication (int num) 
+{
+  int multi = 1;
+  for (int i = 1; i <= num; i++)
+  {
+    checked
+    {
+      multi = multi * i;
+    }
+  
+  }
+  return multi;
+}
