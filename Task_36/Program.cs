0f39bdcc44bebe38efa1781fe6d682495d82612d
«Irina <ira87m@mail.ru>
2023-02-24 23:11:18 +0300

    Task 36

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -4,3 +4,42 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int SumArrayElement (int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+       if (i % 2 != 0) sum = sum + arr[i];
+    }    
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(10,-99,99);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Сумма элементов, стоящих на нечётных позициях = ");
+int result = SumArrayElement (array);
+Console.WriteLine($"{result} ");
