8480e4b6fabc5ae538cf7bacc18cdf5ed3d3cd84
Irina Melianiuk <ira87m@mail.ru>
2023-02-24 16:33:06 +0300

    Task 26

---
+++ Task_26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26
+// Напишите программ, которая принимает на вход число и выдает количество цифр в числе.
+// 456 --3
+// 78 --2
+// 89126 -- 5
+
+int number = new Random().Next();  
+ Console.WriteLine($"Случайное число => {number}");
+
+int result = Count(number);
+Console.WriteLine($"Количество цифр в числе {number} = {result}");
+
+
+int Count(int num)
+{ 
+  int count = 0;
+  while (num > 0)
+    {
+      if (num % 10 > 0)
+      {
+        num = num / 10;
+        count = count + 1; 
+      }
+    }
+  return (count);
+}
+
+
