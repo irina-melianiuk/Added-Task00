58f1e0381e4da4b84c63926d88d7d24f64ed1d0b
«Irina <ira87m@mail.ru>
2023-02-24 22:51:12 +0300

    Task 38

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+double[] CreateArrayRndDouble (int size, int min, int max) 
+{     
+    double[] arr = new double[size];     
+    Random rnd = new Random();     
+    for (int i = 0; i < arr.Length; i++)     
+    {         
+        arr[i] = rnd.NextDouble() * (max - min) + min;     
+    }     
+    return arr; 
+    }  
+    void PrintArrayDouble (double[] arr) 
+    {     
+        Console.Write("[");     
+        for (int i = 0; i < arr.Length; i++)     
+        {         
+            if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");         
+            else Console.Write($"{arr[i]}");     
+            }     
+        Console.Write("]"); 
+    }
