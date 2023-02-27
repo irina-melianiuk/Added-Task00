6d143b6a603c6ab2e179ebe8a46a490d3cdf7e90
Irina Melianiuk <ira87m@mail.ru>
2023-02-27 13:02:27 +0300

    Task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Promt(string message)
+{
+  Console.Write(message);
+  string readInput = Console.ReadLine();
+  int result = int.Parse(readInput);
+  return result;
+}
+
+int SumNumber (int num)
+{
+  int res = 0;
+  while (num > 0)
+  {
+    res = res + num % 10;
+    num = num / 10;
+  }
+  return res;
+}
+int number = Promt ("Введите число ");
+// int sumNumber = SumNumber (number);
+Console.WriteLine($"Сумма цифр числа {number} равна -- {SumNumber (number)}");
