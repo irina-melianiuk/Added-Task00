ab04082bf8deaf03dcd5286fe18fc5ddcfc2c98c
Irina Melianiuk <ira87m@mail.ru>
2023-03-01 16:41:25 +0300

    Task 41

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -2,3 +2,42 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+int[] NewArray(int num)
+{
+  int[] arr = new int[num];
+  for (int i = 0; i < num; i++)
+  {
+    Console.WriteLine($"Введите {i+1} элемент массива: ");
+    int element = Convert.ToInt32(Console.ReadLine());
+    arr[i] = element;
+  }
+  return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+int CheckArray (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (arr[i] > 0) count++;
+    }    
+    return count;
+}
+
+
+Console.WriteLine("Введите количество элементов в массиве");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int[] arr = NewArray(number);
+Console.WriteLine($"Полученный массив из {number} элементов ");
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine($"В массиве присутсвует {CheckArray (arr)} элементов больше нуля.");
+
