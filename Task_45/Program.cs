a8a84f6d3a2eb447de8972b7a3ba95167ada1e03
Irina Melianiuk <ira87m@mail.ru>
2023-03-06 16:31:51 +0300

    Task 45

---
+++ Task_45/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 45: Напишите программу, которая будет создавать 
+// копию заданного массива с помощью пожлементного копирования
+
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] CopyArray(int[] arr)
+{
+  int[] newArr = new int[arr.Length];
+  for (int i = 0; i < arr.Length; i++)
+  {
+    newArr[i] = arr[i];
+  }
+  return newArr;
+}
+
+int[] array = CreateArrayRndInt(5,1,10);
+PrintArray(array);
+Console.WriteLine();
+int[] newArray = CopyArray(array);
+newArray[0] = 99;
+PrintArray(newArray);
