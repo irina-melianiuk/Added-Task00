d3791aab3141b716e9689e03b38165bb1121834e
Irina Melianiuk <ira87m@mail.ru>
2023-03-07 11:35:47 +0300

    Task 48

---
+++ Task_48/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 
+
+int[,] CreateMatrixRndInt(int rows, int columns)
+{
+  int[,] matrix = new int[rows, columns]; // 0 , 1
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++) // rows
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++) // columns
+    {
+      matrix[i, j] = i + j;
+    } 
+  }
+  return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 3}, ");
+      else Console.Write($"{matrix[i, j], 3}");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+int Promt(string message)
+{
+  Console.Write(message);
+  string readInput = Console.ReadLine();
+  int result = int.Parse(readInput);
+  return result;
+}
+
+int matrixRows = Promt("Введите количество строк: ");
+int matrixColumns = Promt("Введите количество строк: ");
+int[,] array2d = CreateMatrixRndInt(matrixRows, matrixColumns);
+PrintMatrix(array2d);
