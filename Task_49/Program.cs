7e5e1923b28a2d315f00e4b2684b9f265fedbbdf
Irina Melianiuk <ira87m@mail.ru>
2023-03-07 13:48:16 +0300

    Task 49

---
+++ Task_49/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+  int[,] matrix = new int[rows, columns]; // 0 , 1
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++) // rows
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++) // columns
+    {
+      matrix[i, j] = rnd.Next(min, max + 1);
+    }
+  }
+  return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 3}, ");
+      else Console.Write($"{matrix[i, j], 3}");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+void ChahgeMatrix(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (i % 2 == 0 && j % 2 == 0)
+      {
+       array[i, j] *= array[i, j];
+      }
+    }
+  }
+  //return array;
+}
+
+int[,] array2d = CreateMatrixRndInt(5, 7, 1, 5);
+PrintMatrix(array2d);
+Console.WriteLine();
+ChahgeMatrix(array2d);
+PrintMatrix(array2d);
