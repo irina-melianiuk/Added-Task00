3415a8607d3a7360e22ed9d8b5d7917fbaf185e8
Irina Melianiuk <ira87m@mail.ru>
2023-03-07 11:44:09 +0300

    Task 48

--- Task_48/Program.cs
+++ Task_48/Program.cs
@@ -6,45 +6,42 @@
 // 1 2 3 4
 // 2 3 4 
 
-int[,] CreateMatrixRndInt(int rows, int columns)
+int[,] CreateMatrixInt(int rows, int columns)
 {
-  int[,] matrix = new int[rows, columns]; // 0 , 1
-  Random rnd = new Random();
-
-  for (int i = 0; i < matrix.GetLength(0); i++) // rows
+  int[,] matrix = new int[rows, columns];
+  for (int i = 0; i < matrix.GetLength(0); i++)
   {
-    for (int j = 0; j < matrix.GetLength(1); j++) // columns
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-      matrix[i, j] = i + j;
-    } 
+      matrix[i, j] = i+j;
+    }
   }
   return matrix;
 }
-
 
 void PrintMatrix(int[,] matrix)
 {
   for (int i = 0; i < matrix.GetLength(0); i++)
   {
-    Console.Write("|");
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-      if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 3}, ");
-      else Console.Write($"{matrix[i, j], 3}");
+      Console.Write($"{matrix[i,j], 3} "); // форматирование строки
     }
-    Console.WriteLine("|");
+  Console.WriteLine();
   }
 }
 
 int Promt(string message)
 {
   Console.Write(message);
-  string readInput = Console.ReadLine();
-  int result = int.Parse(readInput);
+  string value = (Console.ReadLine());
+  int result = Convert.ToInt32(value);
   return result;
 }
 
+Console.Clear();
+
 int matrixRows = Promt("Введите количество строк: ");
 int matrixColumns = Promt("Введите количество строк: ");
-int[,] array2d = CreateMatrixRndInt(matrixRows, matrixColumns);
+int[,] array2d = CreateMatrixInt(matrixRows, matrixColumns);
 PrintMatrix(array2d);
