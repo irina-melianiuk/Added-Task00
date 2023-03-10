708bc108d25b11453a51cc535ba6a895cb401de8
Irina Melianiuk <ira87m@mail.ru>
2023-03-10 11:46:04 +0300

    Task 55

--- Task_55/Program.cs
+++ Task_55/Program.cs
@@ -38,7 +38,7 @@ void Replace(int[,] matrix)
 {
   for (int i = 0; i < matrix.GetLength(0)-1; i++)
     {
-      for (int j = i; j < matrix.GetLength(1); j++)
+      for (int j = i+1; j < matrix.GetLength(1); j++)
       {
         int temp = matrix[i,j];
         matrix[i, j] = matrix[j, i];
