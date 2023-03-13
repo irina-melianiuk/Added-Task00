5de13cf8fbf14aabd26acbb6c46a988bd050a730
Irina Melianiuk <ira87m@mail.ru>
2023-03-13 16:40:56 +0300

    Task 56

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -6,3 +6,41 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+  int[,] matrix = new int[rows, columns]; // 0 , 1
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++) // rows
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++) // columns
+    {
+      matrix[i, j] = rnd.Next(min, max + 1);
+    }
+  }
+  return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+      else Console.Write($"{matrix[i, j],3}");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+
+
+int[,] array2d = CreateMatrixRndInt(3, 3, 1, 5);
+PrintMatrix(array2d);
+Console.WriteLine();
+int sumStringMatrix = SumStringMatrix(array2d);
+Console.Write($"Строка с наименьшей суммой элементов {sumStringMatrix}. ");
+
